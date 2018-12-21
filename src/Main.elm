b2fa6703c7fedab741541683398ca2f05fa40925
Seito Taka <amanoese@gmail.com>
2018-12-21 13:26:17 +0900

    view svg

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Browser
+import List exposing (..)
 import Html exposing (..) --(h1, div, p, text)
 import Html.Attributes exposing (..) -- (class..)
 import Svg as S
@@ -10,8 +11,36 @@ import Bootstrap.Grid as Grid
 import Bootstrap.Grid.Row as Row
 import Bootstrap.Grid.Col as Col
 
+cellsNum: List (List Int)
+cellsNum =
+  repeat 10 (repeat 10 1)
+
+flattenCellsNum: List (List Int) -> List (Int,Int)
+flattenCellsNum =
+  concatMap (indexedMap Tuple.pair)
+
+flattenCellsNumIndex: List (Int,Int) -> List(Int,Int,Int)
+flattenCellsNumIndex =
+  indexedMap (\i (a,b) -> (i,a,b))
+
+type alias Cell = { x:Int , y:Int, v:Int }
+cells:List Cell
+cells =
+  cellsNum
+  |> flattenCellsNum
+  |> flattenCellsNumIndex
+  |> List.map (\(x,y,v)->Cell x y v)
+
+cellToSvgRect: List Cell -> List (S.Svg msg)
+cellToSvgRect =
+  List.map (\cell -> S.rect [ S.x <| String.fromInt <| cell.x * 5, S.y <| String.fromInt <| cell.y * 5  , S.width "5" , S.height "5" ] [] )
+
+
 main =
-  Browser.sandbox { init = Cmd.none, update = update, view = view }
+  Browser.sandbox {
+      init = { cells =  cells }
+    , update = update, view = view 
+    }
 
 update () model = model
 
@@ -25,18 +54,11 @@ view model =
     , Grid.row [ Row.attrs [ class "text-center align-middle" ] ]
         [ Grid.col []
             [ S.svg
-                [ S.width "300"
-                , S.height "300"
-                , S.viewBox "0 0 300 300"
+                [ S.width "500"
+                , S.height "500"
+                , S.viewBox "0 0 500 500"
                 ]
-                [ S.rect
-                  [ S.x "50"
-                  , S.y "50"
-                  , S.width "50"
-                  , S.height "50"
-                  ]
-                  []
-                ]
+                (cellToSvgRect model.cells)
             ]
         ]
     ]
