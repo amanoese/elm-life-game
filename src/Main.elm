4b670cf339b23bb0d7fc126459645d7b96415e85
Seito Taka <amanoese@gmail.com>
2018-12-21 11:09:22 +0900

    add svg

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,8 @@ module Main exposing (..)
 import Browser
 import Html exposing (..) --(h1, div, p, text)
 import Html.Attributes exposing (..) -- (class..)
+import Svg as S
+import Svg.Attributes as S
 import Bootstrap.CDN as CDN
 import Bootstrap.Grid as Grid
 import Bootstrap.Grid.Row as Row
@@ -19,5 +21,22 @@ view model =
     , Grid.row [ Row.attrs [ class "text-center align-middle" ] ]
         [ Grid.col []
             [ div [] [ text "Hello,Elm!" ] ]
+        ]
+    , Grid.row [ Row.attrs [ class "text-center align-middle" ] ]
+        [ Grid.col []
+            [ S.svg
+                [ S.width "300"
+                , S.height "300"
+                , S.viewBox "0 0 300 300"
+                ]
+                [ S.rect
+                  [ S.x "50"
+                  , S.y "50"
+                  , S.width "50"
+                  , S.height "50"
+                  ]
+                  []
+                ]
+            ]
         ]
     ]
